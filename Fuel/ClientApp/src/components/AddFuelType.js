1c9b80abf3fec010ced56c9985b88870acc7eb2b
Сергей <kovbasa.sergey@gmail.com>
2020-02-03 01:46:34 +0200

    Try todo CRUD

---
+++ Fuel/ClientApp/src/components/AddFuelType.js
@@ -0,0 +1,41 @@
+﻿import React, { Component } from 'react';
+
+export class AddFuelType extends Component {
+
+    constructor(props) {
+        super(props);
+        this.fuelType = { code: null, name: null, isActive: true, isHasIncome: false, isHasOutcome: false, isHasRemains: false }
+        this.state = { fuelType: this.fuelType }
+    }
+
+    handleChange(event) {
+        const fuelType = this.state.fuelType;
+        fuelType[event.target.name] = event.target.value;
+        this.setState({ fuelType: fuelType });
+    }
+
+    handleChangeCheckbox(event) {
+        const fuelType = this.state.fuelType;
+        fuelType[event.target.name] = event.target.checked;
+        this.setState({ fuelType: fuelType });
+    }
+
+
+    render() {
+        return (
+            <form>
+                <label>Code</label>
+                <input type="text" value={this.state.fuelType.code} name="code" onChange={this.handleChange.bind(this)}></input>
+                <label>Name</label>
+                <input type="text" value={this.state.fuelType.name} name="name" onChange={this.handleChange.bind(this)} />
+                <input type="checkbox" checked={this.state.fuelType.isActive} name='isActive' onChange={this.handleChangeCheckbox.bind(this)} ></input>
+                <input type="checkbox" checked={this.state.fuelType.isHasIncome} name='isHasIncome' onChange={this.handleChangeCheckbox.bind(this)} />
+                <input type="checkbox" checked={this.state.fuelType.isHasOutcome} name='isHasOutcome' onChange={this.handleChangeCheckbox.bind(this)}/>
+                <input type="checkbox" checked={this.state.fuelType.isHasRemains} name='isHasRemains' onChange={this.handleChangeCheckbox.bind(this)}/>
+                <button>Add new user</button>
+            </form>
+        )
+    }
+}
+
+export default AddFuelType
--- Fuel/ClientApp/src/components/FetchData.js
+++ Fuel/ClientApp/src/components/FetchData.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { AddFuelType } from './AddFuelType';
+import { Link } from 'react-router-dom';
 
 export class FetchData extends Component {
   static displayName = FetchData.name;
@@ -16,41 +18,48 @@ export class FetchData extends Component {
   }
 
   static renderForecastsTable (forecasts) {
-    return (
-      <table className='table table-striped'>
-        <thead>
-          <tr>
-                    <th>fuID</th>
-                    <th>Code</th>
-                    <th>Name</th>
-                    <th>Is active</th>
-                    <th>Is has income</th>
-                    <th>Is has outcome</th>
-                    <th>Is has remains</th>
-                    <th>Create at</th>
-                    <th>Create by</th>
-                    <th>Edit at</th>
-                    <th>Edit by</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.fuID}>
-                  <td>{forecast.fuID}</td>
-                  <td>{forecast.code}</td>
-                  <td>{forecast.name}</td>
-                  <td><input type="checkbox" checked={forecast.isActive} /*onChange={this.handleChange.bind(this, index)}*/ /></td>
-                  <td><input type="checkbox" checked={forecast.isHasIncome} /*onChange={this.handleChange.bind(this, index)}*/ /></td>
-                  <td><input type="checkbox" checked={forecast.isHasOutcome} /*onChange={this.handleChange.bind(this, index)}*/ /></td>
-                  <td><input type="checkbox" checked={forecast.isHasRemains} /*onChange={this.handleChange.bind(this, index)}*/ /></td>
-                  <td>{forecast.createAt}</td>
-                  <td>{forecast.createBy}</td>
-                  <td>{forecast.editAt}</td>
-                  <td>{forecast.editBy}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      return (
+          <div>
+              <Link to='/addFuelType'
+                  type="button"
+                  className="btn login_btn bg-info">
+                  Add
+              </Link>
+              <table className='table table-striped'>
+                <thead>
+                  <tr>
+                            <th>fuID</th>
+                            <th>Code</th>
+                            <th>Name</th>
+                            <th>Is active</th>
+                            <th>Is has income</th>
+                            <th>Is has outcome</th>
+                            <th>Is has remains</th>
+                            <th>Create at</th>
+                            <th>Create by</th>
+                            <th>Edit at</th>
+                            <th>Edit by</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {forecasts.map(forecast =>
+                    <tr key={forecast.fuID}>
+                          <td>{forecast.fuID}</td>
+                          <td>{forecast.code}</td>
+                          <td>{forecast.name}</td>
+                          <td><input type="checkbox" checked={forecast.isActive} /*onChange={this.handleChange.bind(this, index)}*/ /></td>
+                          <td><input type="checkbox" checked={forecast.isHasIncome} /*onChange={this.handleChange.bind(this, index)}*/ /></td>
+                          <td><input type="checkbox" checked={forecast.isHasOutcome} /*onChange={this.handleChange.bind(this, index)}*/ /></td>
+                          <td><input type="checkbox" checked={forecast.isHasRemains} /*onChange={this.handleChange.bind(this, index)}*/ /></td>
+                          <td>{forecast.createAt}</td>
+                          <td>{forecast.createBy}</td>
+                          <td>{forecast.editAt}</td>
+                          <td>{forecast.editBy}</td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+          </div>
     );
   }
 
